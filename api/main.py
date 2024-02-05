41fedb71419c81794e3dbfcf45a917e6ed463cae
nikoyasam <nikosamaranayake16@gmail.com>
2024-02-05 15:57:30 +0530

    Fast api implementation

--- api/main.py
+++ api/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, UploadFile,File
 import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
 
 #create app which is an instance of fast api
 app = FastAPI()
@@ -9,12 +12,19 @@ app = FastAPI()
 async def ping():
     return "Hello, I am alive"
 
+# Define a function to read a file as an image
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
 # Define a route for making predictions using a file upload
 @app.post("/predict")
 async def predict(
     file: UploadFile = File(...)
 ):
-    pass
+    # Read the uploaded file as an image
+    image = read_file_as_image(await file.read())
+    return
 # Run the FastAPI application using Uvicorn
 if __name__ == "__main__":
     uvicorn.run(app, host='localhost', port=8000)
