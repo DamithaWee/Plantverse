44b1fdfaed792b9a8084522fba47bfa22f8dc0ce
YasiniK <yasinikarunanayake@gmail.com>
2024-03-14 13:35:44 +0530

    Chatbot File Changes

--- Rasa Chatbot/pesti bot/actions/actions.py
+++ Rasa Chatbot/pesti bot/actions/actions.py
@@ -3,32 +3,14 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import UserUtteranceReverted
 
-class ActionGreet(Action):
+class ActionFallback(Action):
     def name(self) -> Text:
-        return "utter_greet"
+        return "action_default_fallback"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(template="utter_greet")
-        return []
-
-class ActionThankYou(Action):
-    def name(self) -> Text:
-        return "utter_thank_you"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(template="utter_thank_you")
-        return []
-
-class ActionGoodbye(Action):
-    def name(self) -> Text:
-        return "utter_goodbye"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(template="utter_goodbye")
-        return []
+            last_user_message = tracker.get_last_event_for("user")
+            if last_user_message:
+                dispatcher.utter_message("I'm sorry, I didn't understand. Can you please rephrase your message?")
+                return [UserUtteranceReverted()]
