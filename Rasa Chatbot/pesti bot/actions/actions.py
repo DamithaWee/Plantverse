32465bbede0e5316c1d0ed3e067025d8d35314a8
YasiniK <yasinikarunanayake@gmail.com>
2024-03-16 18:32:17 +0530

    Chatbot Chanages

--- Rasa Chatbot/pesti bot/actions/actions.py
+++ Rasa Chatbot/pesti bot/actions/actions.py
@@ -3,6 +3,7 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import UserUtteranceReverted
 
+
 class ActionFallback(Action):
     def name(self) -> Text:
         return "action_default_fallback"
@@ -12,5 +13,7 @@ class ActionFallback(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
             last_user_message = tracker.get_last_event_for("user")
             if last_user_message:
-                dispatcher.utter_message("I'm sorry, I didn't understand. Can you please rephrase your question?")
+                dispatcher.utter_message("I'm sorry, I didn't understand. Can you please rephrase your message?")
                 return [UserUtteranceReverted()]
+
+
